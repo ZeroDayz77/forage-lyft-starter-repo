d7cf445875c728f71298a846cf920e5b78a0b689
zerodayz77 <charisyt26@gmail.com>
2023-05-28 20:47:51 -0400

    Wow... i am left speechless but now i will never forget how to pass an array ( list ) as an arg in python lol

--- tyre/carrigan_tyre.py
+++ tyre/carrigan_tyre.py
@@ -2,15 +2,12 @@ from tyre.tyre import Tyre
 
 class CarriganTyre(Tyre):
     def __init__(self, tyre_wear):
-        self.tyre_wear = tyre_wear[0] * 4
+        self.tyre_wear = tyre_wear
     
     
     def needs_service(self):
-        sum = 0
-        
+    
         for x in self.tyre_wear:
-            sum += x
-        if sum >= 3:
-            return True
-        else:
-            return False
+            if x >= 0.9:
+                return True
+        return False
--- tyre/octoprime_tyre.py
+++ tyre/octoprime_tyre.py
@@ -2,12 +2,15 @@ from tyre.tyre import Tyre
 
 class OctoprimeTyre(Tyre):
     def __init__(self, tyre_wear):
-        self.tyre_wear = tyre_wear[0,0,0,0]
+        self.tyre_wear = tyre_wear
     
     
     def needs_service(self):
+        sum = 0
+        
         for x in self.tyre_wear:
-            if x >= 0.9:
-                return True
-            else:
-                return False
+            sum += x
+        if sum >= 3:
+            return True
+        else:
+            return False
